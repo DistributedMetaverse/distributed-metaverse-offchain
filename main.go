8311c73527878b3c897f3c4efe05d6eacc142456
Namhyeon, Go <gnh1201@gmail.com>
2022-09-10 15:55:04 +0900

    Update main.go

--- main.go
+++ main.go
@@ -4,9 +4,11 @@ import (
     "context"
     "fmt"
     "time"
+    "strconv"
+    "encoding/json"
+    "crypto/sha256"
     "github.com/labstack/echo/v4"
     "github.com/labstack/echo/v4/middleware"
-    "net/http"
     "github.com/go-redis/redis/v8"   
 )
 
@@ -31,7 +33,7 @@ type Transaction struct {
 }
 
 func (b Block) calculateHash() string {
-    data, _ := json.Marshal(b.data)
+    data, _ := json.Marshal(b.transactions)
     blockData := b.previousHash + string(data) + b.timestamp.String() + strconv.Itoa(b.proof)
     blockHash := sha256.Sum256([]byte(blockData))
     return fmt.Sprintf("%x", blockHash)
@@ -39,20 +41,13 @@ func (b Block) calculateHash() string {
 
 
 // 블록 추가할 때
-func (b *Blockchain) addBlock() string {
-    
+func (b *Blockchain) addBlock() {
 }
-
-// 블록이 분기되었을 때 머지(병합) 방법 정의
-func (b *Blockchain) margeBlock(c1 Block, c2 Block) Block {
-    
-}
-
 
 
 // 증명 절차 정의
 func doProof() {
-       
+    
 }
 
 // 외부 API
@@ -72,78 +67,114 @@ func doServe() {
 }
 
 // 트랜젝션 빌드
-func buildTransaction(data string) Transaction {
-	lastTransactionId := getLastTransactionId()
+func buildTransaction(data string) (Transaction, error) {
+    lastTransactionId, err := getLastTransactionId()
 
-	if lastTransactionId < 0 {
-		return nil
-	}
+    if lastTransactionId < 0 {
+        return Transaction{}, err
+    }
 
-	transaction := Transaction{
-	    data: data,
-		id: lastTransactionId + 1,
-		timestamp: time.Now()
-	}
-	
-	return transaction
+    transaction := Transaction{
+        data: data,
+        id: lastTransactionId + 1,
+        timestamp: time.Now(),
+    }
+
+    return transaction, nil
 }
 
 // 최근 트랜젝션 ID 구하기
-func getLastTransactionId() int {
-	lastTransactionId := -1
+func getLastTransactionId() (int, error) {
+    lastTransactionId := -1
 
     result, err := rdb.Get(ctx, "lastTransactionId").Result()
     if err != nil {
-        return lastTransactionId
+        return lastTransactionId, err
     }
-	lastTransactionId = strconv.Atoi(result)
 
-	return lastTransactionId
+    id, err := strconv.Atoi(result)
+    if err != nil {
+        lastTransactionId = id
+    } else {
+        err := rdb.Set(ctx, "lastTransactionId", "0", 0).Err()
+        if err != nil {
+            lastTransactionId = 0
+        }
+    }
+
+    return lastTransactionId, err
+}
+
+// 최근 블록 해시 구하기
+func getLastBlockHash() (string, error) {
+    lastBlockHash := ""
+
+    result, err := rdb.Get(ctx, "lastBlockHash").Result()
+    if err != nil {
+        return lastBlockHash, err
+    } else {
+        lastBlockHash = result
+    }
+
+    return lastBlockHash, nil
 }
 
 // 시작점(Entrypoint)
 func main() {
-    // Connect to Redis server
+    // Redis 서버에 연결
     rdb = redis.NewClient(&redis.Options{
         Addr:     "154.12.242.48:60713",
         Password: "", // no password set
         DB:       0,  // use default DB
     })
-	
-	// doProof()
-	// doServe()
+    
+    // 최근 트랜젝션 ID 초기화
+    getLastTransactionId()
+    
+    
+    
+    // doProof()
+    // doServe()
 }
 
 // 트랜젝션 발행
 func publish(c echo.Context) error {
-	// 받은 요청 해석
-	jsonMap := make(map[string]interface{})
-	e1 := json.NewDecoder(c.Request().Body).Decode(&jsonMap)
-	if e1 != nil {
-		return e1
-	}
+    // 받은 요청 해석
+    jsonMap := make(map[string]interface{})
+    err := json.NewDecoder(c.Request().Body).Decode(&jsonMap)
+    if err != nil {
+        return err
+    }
 
-	// 변수로 재정렬
-	hash := jsonMap["hash"]
-	filetype := jsonMap["filetype"]
-	filename := jsonMap["filename"]
+    // 변수로 재정렬
+    hash := jsonMap["hash"].(string)
+    filetype := jsonMap["filetype"].(string)
+    filename := jsonMap["filename"].(string)
 
-	// 신규 트랜젝션 생성
-	newTransaction := buildTransaction(hash + "\t" + filetype + "\t" + filename)
+    // 신규 트랜젝션 생성
+    transaction, err := buildTransaction(hash + "\t" + filetype + "\t" + filename)
+    if err != nil {
+        return err
+    }
 
-	// 발행할 트랜젝션 본문 만들기
-	publishData, _ := json.Marshal(newTransaction)
-	
-	// 트랜젝션 발행
-	e2 := rdb.Publish(ctx, "transaction_live", publishData).Err()
-	if e2 != nil {
-	    return e2
-	}
+    // 발행할 트랜젝션 본문 만들기
+    publishJsonData, err := json.Marshal(transaction)
+    if err != nil {
+        return err
+    }
+
+    // 트랜젝션 발행
+    err2 := rdb.Publish(ctx, "transactions_live", publishJsonData).Err()
+    if err2 != nil {
+        return err2
+    }
+    
+    // 모든 작업이 완료되었으면 오류 없음으로 반환
+    return nil
 }
-
-
 
 // References:
 // https://gist.github.com/LordGhostX/bb92b907731ee8ebe465a28c5c431cb4
 // https://redis.uptrace.dev/guide/go-redis-pubsub.html
 // https://stackoverflow.com/questions/41410655/extract-json-from-golangs-echo-request
+// https://stackoverflow.com/questions/27137521/how-to-convert-interface-to-string
